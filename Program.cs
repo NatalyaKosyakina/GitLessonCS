9bed09b5568160e124f77b250f9bbd4f2e292822
natalya_kosyakina <anckamarckowa@yandex.ru>
2023-12-03 13:48:56 +0300

    некий третий коммит

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Bits bits = new(36.5);
+
+            Console.WriteLine(bits);
+            byte val = (byte)bits;
+            Console.WriteLine(val);
+        }
+    }
+}
